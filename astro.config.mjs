0b67ce024c89fd9a23ff95bb88794f35387f1068
Alejo <alejocstaf@proton.me>
2025-01-13 14:08:49 -0300

    configuracion de vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,8 @@ import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tu-dominio-en-vercel.vercel.app', // Esto se actualizará automáticamente cuando despliegues
   output: 'server', // Necesario para SSR en Vercel
   adapter: vercel(), // Añadimos el adaptador de Vercel
-
-  server: {
-    port: process.env.PORT || 2121,
-  },
 
   integrations: [
     sitemap(),
